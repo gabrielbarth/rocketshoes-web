f998bf233f962254d32fb47fcf100493534d779e
gabrielbarth <gabrielbarth98@gmail.com>
2020-03-17 07:40:23 -0400

    first commit

---
+++ src/store/modules/cart/actions.js
@@ -0,0 +1,37 @@
+// received by user action and send to redux-saga
+export function addToCartRequest(id) {
+  return {
+    type: '@cart/ADD_REQUEST',
+    id,
+  };
+}
+// received by redux-saga and send to redux (store) to change state
+export function addToCartSuccess(product) {
+  return {
+    type: '@cart/ADD_SUCCESS',
+    product,
+  };
+}
+
+export function removeFromCart(id) {
+  return {
+    type: '@cart/REMOVE',
+    id,
+  };
+}
+
+export function updateAmountRequest(id, amount) {
+  return {
+    type: '@cart/UPDATE_AMOUNT_REQUEST',
+    id,
+    amount,
+  };
+}
+
+export function updateAmountSuccess(id, amount) {
+  return {
+    type: '@cart/UPDATE_AMOUNT_SUCCESS',
+    id,
+    amount,
+  };
+}
---
+++ src/store/modules/cart/sagas.js
@@ -0,0 +1,72 @@
+import { call, put, all, takeLatest, select } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
+import api from '../../../services/api';
+import history from '../../../services/history';
+import { formatPrice } from '../../../util/format';
+
+import { addToCartSuccess, updateAmountSuccess } from './actions';
+
+function* addToCart({ id }) {
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    toast.error('Amount requested out of stock');
+    return;
+  }
+
+  if (productExists) {
+    yield put(updateAmountSuccess(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+
+    history.push('/cart');
+  }
+}
+
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Amount requested out of stock');
+    return;
+  }
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
+
+/**
+ * (* / yield) it's like asynct/await, but contains more functionalities
+ * call -> method to call to api
+ * put -> method to dispatch a action to redux (store) -> reducer -> changing
+ * app state
+ * all -> allows join sagas
+ * takeLatest -> allows listen just the last call to api (useful when user do
+ * the same request several times)
+ * takeLatest('<LISTENED-REDUX-ACTION>', <ACTION-TO-BE-DISPATCH>)
+ * select -> allows seaching states info
+ */
